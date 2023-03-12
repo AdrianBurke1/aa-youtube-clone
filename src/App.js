dd581415c071dc0fa8aa475a0e93a0a9e39fe3b8
Adrian Burke <114108495+AdrianBurke1@users.noreply.github.com>
2023-03-12 14:47:46 -0400

    nav icon created

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import './App.css';
-import home from './Components/home';
-import navbar from './Components/Navbar';
-import videoIndex from './Components/Videosindex';
-import videoListing from './Components/videoListing';
+import "./App.css";
+import React from "react";
+import Header from "./Components/Header";
+import home from "./Components/home";
+import Navbar from "./Components/navbar";
+import videoIndex from "./Components/videosIndex";
+import videoListing from "./Components/videoListing";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title"> </h1>
+
+      <home />
+      <Header />
+      <navBar />
+      <videoIndex />
+      <videoListing />
+      <videoEdit />
     </div>
   );
 }
 
 export default App;
+
+// This is being done in my branch only push !!!
+// Git add .
+// Git commit -m “What I did”
+// Git push origin Adrian-branch
+// ------------------------------
+// ONLY PULL IN MAIN !!!!
+// Git checkout main
+// Git pull origin main
