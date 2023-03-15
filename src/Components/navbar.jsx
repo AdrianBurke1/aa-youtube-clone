adc255d77a334eb866e634eaa7b022676a6a6d48
Ari-So-Irie <arius1500@gmail.com>
2023-03-15 18:20:15 -0400

    merge changes

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,10 +1,27 @@
-function navbar (){
+function Navbar (){
 
     return (
-        <div>
-
+        <div className="header d-flex align-items-center justify-content-space-between">
+        <div className="header-left-itmes d-flex align-items-center mxy-30">
+          <div className="menubar">
+            <svg
+              viewBox="0 0 24 24"
+              preserveAspectRatio="xMidYMid meet"
+              focusable="false"
+              class="style-scope yt-icon"
+              style={{"width" : "30px" , "height" : "30px"}}
+            >
+              <g class="style-scope yt-icon">
+                <path
+                  d="M21,6H3V5h18V6z M21,11H3v1h18V11z M21,17H3v1h18V17z"
+                  class="style-scope yt-icon"
+                ></path>
+              </g>
+            </svg>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
-export default navbar;
+export default Navbar;
