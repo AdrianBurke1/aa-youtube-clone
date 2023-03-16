1b2f01e9c833172542bc4f57f105112284aaf96c
Adrian Burke <114108495+AdrianBurke1@users.noreply.github.com>
2023-03-15 21:36:51 -0400

    update changes

--- src/VideoListing.js
+++ src/VideoListing.js
@@ -1,11 +1,25 @@
 import React from "react"
+import VideoCard from "./VideoCard";
+import "./VideoListing.css"
 
 function VideoListing (){
     
     return(
         <div className="videoListing">
-        <h2>Recommened</h2>
+            <h2>Recommended</h2>
+         <div className="videoListing_videos">
+          <VideoCard />
+          <VideoCard />
+          <VideoCard />
+          <VideoCard />
+          <VideoCard />
+          <VideoCard />
+          <VideoCard />
+          <VideoCard />
+         </div>
         </div>
+        
+        
     )
 }
 export default VideoListing;
